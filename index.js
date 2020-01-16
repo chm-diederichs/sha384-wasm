e51709395735e7e27ea1afcfe3e0507189744dd2
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-01-16 15:21:06 +0100

    updates and digest may specify encoding

--- index.js
+++ index.js
@@ -26,26 +26,27 @@ function Sha384 () {
 
   if (!freeList.length) {
     freeList.push(head)
-    head += 512
+    head += 200
   }
 
   this.finalized = false
-  this.digestLength = 64
+  this.digestLength = hashLength
   this.pointer = freeList.pop()
   this.leftover
 
-  wasm.memory.fill(0, this.pointer, this.pointer + 512)
+  wasm.memory.fill(0, this.pointer, this.pointer + 200)
 
-  if (this.pointer + hashLength > wasm.memory.length) wasm.realloc(this.pointer + 512)
-  
-  // wasm.exports.sha384_init(0 , this.digestLength) //(this.pointer, this.digestLength)
+  if (this.pointer + hashLength > wasm.memory.length) wasm.realloc(this.pointer + 200)
 }
 
-Sha384.prototype.update = function (input) {
-  // assert input % 8 === 0 for alignment
-
-  let [ inputBuf, length ] = formatInput(input)
+Sha384.prototype.update = function (input, enc) {
   assert(this.finalized === false, 'Hash instance finalized')
+
+  if (head % 8 !== 0) head += 8 - head % 8
+  assert(head % 8 === 0, 'input should be aligned for int64')
+
+  let [ inputBuf, length ] = formatInput(input, enc)
+
   assert(inputBuf instanceof Uint8Array, 'input must be Uint8Array or Buffer')
 
   if (head + input.length > wasm.memory.length) wasm.realloc(head + input.length)
@@ -64,7 +65,7 @@ Sha384.prototype.update = function (input) {
   return this
 }
 
-Sha384.prototype.digest = function (enc) {
+Sha384.prototype.digest = function (enc, offset = 0) {
   assert(this.finalized === false, 'Hash instance finalized')
   this.finalized = true
 
@@ -72,19 +73,21 @@ Sha384.prototype.digest = function (enc) {
 
   wasm.exports.sha384_monolith(this.pointer, head, head + this.leftover.byteLength, 1)
 
-  // if (!enc || end === 'binary') {    
-  //   return wasm.memory.slice(this.pointer, this.pointer + 32)
-  // }
-
-  return int64reverse(wasm.memory, this.pointer, 48)
-
-  if (enc === 'hex') {
-    return hexSlice(wasm.memory, this.pointer, 32)
+  const resultBuf = int64reverse(wasm.memory, this.pointer, this.digestLength)
+  
+  if (!enc) {
+    return resultBuf
   }
 
-  assert(enc instanceof Uint8Array && enc.length >= 32, 'input must be Uint8Array or Buffer')
-  for (let i = 0; i < 32; i++) {
-    enc[i] = wasm.memory[this.pointer + 32 + i]
+  if (typeof enc === 'string') {
+    return resultBuf.toString(enc)
+  }
+
+  assert(enc instanceof Uint8Array, 'input must be Uint8Array or Buffer')
+  assert(enc.byteLength >= this.digestLength + offset, 'input must be Uint8Array or Buffer')
+
+  for (let i = 0; i < this.digestLengt; i++) {
+    enc[i + offset] = resultBuf[i]
   }
 
   return enc
@@ -94,7 +97,7 @@ Sha384.ready = function (cb) {
   if (!cb) cb = noop
   if (!wasm) return cb(new Error('WebAssembly not supported'))
 
-  var p = new Promise(function (reject, result) {
+  var p = new Promise(function (reject, resolve) {
     wasm.onload(function (err) {
       if (err) resolve(err)
       else reject()
@@ -109,43 +112,22 @@ Sha384.prototype.ready = Sha384.ready
 
 function noop () {}
 
-function formatInput (input) {
-  const value = new Uint8Array(Buffer.from(input))
-  return [value, value.byteLength]
+function formatInput (input, enc) {
+  const inputBuf = Buffer.from(input, enc)
+  const result = new Uint8Array(inputBuf)
 
-  if (input instanceof Uint8Array) return input
-
-  const inputArray = new Uint32Array(Math.ceil(input.length / 4))
-
-  const buf = Buffer.alloc(inputArray.byteLength)
-  buf.set(Buffer.from(input, 'utf8'), 0)
-
-  let i = 0
-
-  for (; i < buf.byteLength / 4; i++) {
-    inputArray[i] = buf.readUInt32LE(4 * i)
-  }
-
-  return [
-    new Uint8Array(inputArray.buffer),
-    input.length
-  ]
+  return [result, result.byteLength]
 }
 
 function int64reverse (buf, start, len) {
-  var str = ''
-  var chars = []
+  const result = new Uint8Array(len)
 
   for (let i = 0; i < len; i++) {
-    chars.push(toHex(buf[start + i]))
-
-    if ((i + 1) % 8 === 0) {
-      str += chars.reverse().join('')
-      chars = []
-    }
+    const index = Math.floor(i / 8) * 8 + 7 - i % 8
+    result[index] = buf[i + start]
   }
 
-  return str
+  return Buffer.from(result)
 }
 
 function hexSlice (buf, start, len) {
